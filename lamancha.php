34e4afaefe2010910194a65669e0e2f1e260888f
Pierre Böckmann <pb@crowbyte.org>
2024-08-24 22:41:01 +0200

    Fixed theme php file naming

--- lamancha.php
+++ lamancha.php
@@ -4,7 +4,7 @@ namespace Grav\Theme;
 use Grav\Common\Grav;
 use Grav\Common\Theme;
 
-class Quark extends Theme
+class LaMancha extends Theme
 {
     public static function getSubscribedEvents()
     {
